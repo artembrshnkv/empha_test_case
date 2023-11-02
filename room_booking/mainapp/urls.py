1571c665d95fb5e1819a1ff253ed9ad6647e3ea2
ArtemBaryshnikov <baryshnikov-artem@inbox.ru>
2023-11-02 20:52:09 +0300

    Check new branch

--- room_booking/mainapp/urls.py
+++ room_booking/mainapp/urls.py
@@ -1,19 +1,19 @@
-from django.urls import path
-
-from .views import *
-
-
-urlpatterns = [
-    path('', RoomList.as_view(), name='main_page'),
-    path('reg', UserRegistration.as_view(), name='registration'),
-    path('account', UserAccount.as_view(), name='account'),
-    path('user_login', UserLogin.as_view(), name='user_login'),
-    path('user_logout', user_logout, name='user_logout'),
-    path('show_room/<slug:slug>', ShowRoom.as_view(), name='show_room'),
-    path('book_room/<int:room_number>', BookRoom.as_view(), name='book_room'),
-    path(
-        'delete_booking/<int:pk>',
-        DeleteBooking.as_view(),
-        name='delete_booking'),
-    # path('choose_date', SortByAvailableDates.as_view(), name='choose_date'),
-]
+# from django.urls import path
+#
+# from .views import *
+#
+#
+# urlpatterns = [
+#     path('', RoomList.as_view(), name='main_page'),
+#     path('reg', UserRegistration.as_view(), name='registration'),
+#     path('account', UserAccount.as_view(), name='account'),
+#     path('user_login', UserLogin.as_view(), name='user_login'),
+#     path('user_logout', user_logout, name='user_logout'),
+#     path('show_room/<slug:slug>', ShowRoom.as_view(), name='show_room'),
+#     path('book_room/<int:room_number>', BookRoom.as_view(), name='book_room'),
+#     path(
+#         'delete_booking/<int:pk>',
+#         DeleteBooking.as_view(),
+#         name='delete_booking'),
+#     # path('choose_date', SortByAvailableDates.as_view(), name='choose_date'),
+# ]
